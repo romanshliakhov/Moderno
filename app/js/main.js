587bd56384cc35ab4f1ed49c6adbb02852a5ecce
romanshliakhov <romanshliakhov@gmail.com>
2020-08-03 00:10:32 +0300

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,19 @@
 $(function(){    
-    
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+      });
+
+    $('.products__slider-inner').slick({
+      dots: true,
+      arrows: false,
+      slidesToShow: 4,
+      slidesToScroll: 4      
+    });
+
+      
+
     
 
 
